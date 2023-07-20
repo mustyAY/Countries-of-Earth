488ae99325e480667cccd984790757cb1837dc47
Mustapha <mustyyahya.1580@yahoo.com>
2023-07-21 00:35:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+ async function renderCountries(){
+    const countries = await fetch("https://restcountries.com/v3.1/all");
+    const countriesData = await countries.json();
+    const countriesEl = document.querySelector('.countries');
+    countriesEl.innerHTML = countriesData.map(country => countryHTML(country)).join(""); 
+    console.log(countriesData);
+ }
+
+ renderCountries();
+
+ function countryHTML(country){
+  return `<div class="country">
+      <figure class="country__img--wrapper">
+      <div class="country__img--bg">${country.name.common}</div>
+      <img src="${country.flags.svg}" alt="" class="country__img">
+      </figure>
+   </div>`
+ }
